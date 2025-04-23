4a86e080fbebfa7288242a6b3070f9cac4d56aec
Erick <efm.physics@gmail.com>
2025-04-23 01:23:29 -0700

    ui improvements!

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import streamlit as st
 from cinemaclub.cc import get_feed, get_user, rounded_img
-from cinemaclub.style import single_image, dual_image
+from cinemaclub.style import single_image, dual_image, feed_image
 
 st.set_page_config(page_title="CinemaClub")
 query_params = st.query_params
 if query_params:
     with st.spinner('Loading...'):
         user = get_user(query_params['username'].lower())
-
     st.markdown(dual_image(user), unsafe_allow_html=True)
     st.write('')
-
     with st.spinner('Loading...'):
         feed = get_feed(query_params['username'])
-    st.write(f"pulled {feed['total_logs']} logs")
     for idx, (log_id, log) in enumerate(feed["logs"].items()):
         st.write('\n\n\n')
+        feed_user = get_user(log.get('username'))
         if log["event_type"] == 'review':
-            st.write(log_id, log.get("title"), log.get("film"))
-            st.write(log.get("review"))
+            text = log.get("title") + ' ' + log.get("film")
+            st.markdown(feed_image(feed_user, text, log.get("review")), unsafe_allow_html=True)
         else:
-            st.write(log_id, log.get("title"))
+            st.markdown(feed_image(feed_user, log.get("title")), unsafe_allow_html=True)
         if idx == 20:  # limit preview to 10 lines
             break
 else:
--- cinemaclub/style.py
+++ cinemaclub/style.py
@@ -101,3 +101,59 @@ single_image = f"""
         </div>
     </div>
     """
+
+
+def feed_image(user, text, subtext='', img_weight=1, text_block_weight=9):
+    row_class = f"feed_row_{user.username.replace('-', '_')}"
+    if subtext:
+        subtext = '"' + subtext + '"'
+    text_html = f"<h5 style='margin: 0;'>{text}</h4>" \
+                f"<p style='margin-top: 0.25rem;'>{subtext}</p>"
+
+    return f"""
+<style>
+.{row_class}-container {{
+    display: flex;
+    justify-content: left;
+    align-items: top;
+    max-width: 600px;
+    gap: 1rem;
+    overflow: hidden;
+    flex-wrap: nowrap;
+}}
+
+.{row_class}-image, .{row_class}-text {{
+    flex-shrink: 1;
+    flex-basis: 0;
+    min-width: 0;
+}}
+
+.{row_class}-image {{
+    flex-grow: {img_weight};
+}}
+
+.{row_class}-text {{
+    flex-grow: {text_block_weight};
+}}
+
+.{row_class}-container img {{
+    width: 100%;
+    height: auto;
+    border-radius: 15px;
+    display: block;
+    object-fit: contain;
+    max-width: 100%;
+}}
+</style>
+
+<div class="{row_class}-container">
+    <a href="https://letterboxd.com/{user.username}" target="_blank" class="{row_class}-image">
+        <img src="{user.avatar['url']}">
+    </a>
+    <div class="{row_class}-text">
+        {text_html}
+    </div>
+</div>
+"""
+
+
